02da3da2549f42d8ccadff24d391b1fdd76fbd0b
Enes Furkan SAGLAM <enesfurkansaglam@gmail.com>
2024-02-23 11:37:00 +0300

    UI design started

--- Source Code/src/Main.java
+++ Source Code/src/Main.java
@@ -1,6 +1,38 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import javax.sound.sampled.*;
 
+public class Main extends JFrame {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().setBackground(Color.BLACK);
+
+        window.setLayout(new FlowLayout());
+        JLabel label = new JLabel("Merhaba, Dünya!");
+        label.setBackground(Color.WHITE);
+        label.setFont(new Font("Arial", Font.BOLD, 150));
+        window.add(label);
+
+        window.setResizable(false);
+        window.setTitle("OTONOM HAZİNE AVCISI");
+        window.setSize(1600, 1000);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        try {
+            File soundFile = new File("C:\\BEN\\Kodlar\\Proje\\Proje_9_Uni_ProLab2_1\\Source Code\\mp3\\karayip.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 }
